fd3538b4e701aa65e2a8f1bf386adaabe731cbb8
KBBOtherspaces <kristinbauerart@gmail.com>
2025-09-04 17:40:16 -0700

    Update route.ts
    line 19 model specification

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613: Spider season',
+    model: 'gpt-3.5-turbo-1106', output model: ft:gpt-3.5-turbo-1106:kbbotherspaces:summerspider:CCDmwGsQ , 
     stream: true,
     messages: [
       {
